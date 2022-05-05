5c33e98707a0861c13bda0a39f7e4ce31569e4be
Jonas Wanner <jonas.wanner@clavisit.com>
2022-05-05 14:59:50 +0200

    feat: implement useful test samples for global edge cache test

--- test/unsort.test.ts
+++ test/unsort.test.ts
@@ -1,7 +1,7 @@
 import { assert, describe, it } from 'vitest'
 import { unsort } from '../src/mod'
 
-describe.concurrent('simple unsort', () => {
+describe.concurrent('unsort without global cache', () => {
   it('numbers between 1 and 3', async () => {
     const sorted = await unsort([2,1,3], { useGlobalEdgeCache: false })
     assert.deepEqual(sorted, [1,2,3])
@@ -23,30 +23,24 @@ describe.concurrent('simple unsort', () => {
   })
 })
 
-describe.concurrent('medium unsort', () => {
-  it('100 random numbers between 1 and 1000', async () => {
-    const unsorted = []
-    for (let i = 0; i < 20; i++) {
-      unsorted.push(Math.floor(Math.random() * 1000))
-    }
-    const sorted = await unsort(unsorted, { useGlobalEdgeCache: false })
-    assert.deepEqual(sorted, unsorted.sort())
-  }, 11000)
-})
-
 describe.concurrent('unsort with global cache', () => {
-  it('numbers between 1 and 3 with cache hit', async () => {
+  it('numbers between 1 and 3', async () => {
     const sorted = await unsort([2,1,3])
     assert.deepEqual(sorted, [1,2,3])
   })
 
-  it('numbers between 1 and 3 without cache hit', async () => {
-    const sorted = await unsort([3,2,1])
-    assert.deepEqual(sorted, [1,2,3])
-  })
-
-  it('numbers between 100 and 300 without cache hit', async () => {
+  it('numbers between 100 and 300', async () => {
     const sorted = await unsort([300,200,100])
     assert.deepEqual(sorted, [100,200,300])
+  })
+
+  it('numbers between 1000 and 3000', async () => {
+    const sorted = await unsort([2000,1000,3000])
+    assert.deepEqual(sorted, [1000,2000,3000])
+  })
+
+  it('numbers between 1 and 3000', async () => {
+    const sorted = await unsort([200,1,3000])
+    assert.deepEqual(sorted, [1,200,3000])
   })
 })
