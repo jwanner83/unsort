c1f16db7cbd16f730f64cc4dd0f37329c99971c9
Jonas Wanner <jonas.wanner@clavisit.com>
2022-05-05 17:33:44 +0200

    feat: add usage example

---
+++ example/mod.js
@@ -0,0 +1,10 @@
+import { unsort } from 'unsort'
+
+(async () => {
+  const unsorted = []
+  for (let i = 0; i < 20; i++) {
+    unsorted.push(Math.floor(Math.random() * 1000))
+  }
+  const sorted = await unsort(unsorted)
+  console.log('unsorted', sorted)
+})()
